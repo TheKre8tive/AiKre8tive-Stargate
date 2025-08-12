0a93b1d50676e842f93f7ca53554c0977203aace
Cygel White <cygel.co@gmail.com>
2025-08-12 18:37:42 -0400

    build: NDA-gated Sovereign Stargate landing (GammaRay) + health APIs

---
+++ api/hello.js
@@ -0,0 +1,1 @@
+export default (req, res) => res.status(200).json({ msg: "Hello from SovereignGTP Planetary Agents" });
---
+++ api/version.js
@@ -0,0 +1,1 @@
+export default (req, res) => res.status(200).json({ version: "1.0.0" });
